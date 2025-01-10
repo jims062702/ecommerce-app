b9cce6a22f33308bf6b4e5256a1b93f77c322267
jims062702 <gasang143x@gmail.com>
2025-01-10 11:14:08 +0800

    my first commit

---
+++ frontend/src/context/ShopContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useState } from "react";
+import { products } from "../assets/assets";
+
+export const ShopContext = createContext();
+
+const ShopContextProvider = (props) => {
+
+    const currency = '₱';
+    const delivery_fee = 10;
+    const [token,setToken] = useState('')
+
+    const value = {
+        products , currency, delivery_fee,setToken,token
+    }
+
+    return (
+        <ShopContext.Provider value={value}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+
+}
+
+export default ShopContextProvider;
